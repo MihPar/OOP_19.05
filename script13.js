4bf4f7e5cfc8daeb750ed87b124ca66566c2fb18
MihPar <9496260@gmail.com>
2023-05-19 15:38:17 +0300

    one

---
+++ script13.js
@@ -0,0 +1,15 @@
+function Dog(name) {
+    this.name = name;
+  }
+  
+  Dog.prototype = {
+    // Only change code below this line
+    numLegs: 4,
+    eat() {
+      console.log("nym, nym")
+    },
+    describe: function() {
+      console.log(`It has ${this.numLegs}`)
+    }
+  
+  };
